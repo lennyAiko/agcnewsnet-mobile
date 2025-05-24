9e590cbfb9acffb4692eeb4303e5c287e14e7121
lennyaiko <lennyaiko17@gmail.com>
2025-05-24 19:02:01 +0100

    progress on stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:agcnews/views/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +10,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
     );
   }
---
+++ lib/views/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: SingleChildScrollView(child: Column(children: [Text("AGC News")])),
+    );
+  }
+}
