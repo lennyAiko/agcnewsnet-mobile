49394d3df60744bc040dc4ddce94c675ff4e374f
lennyaiko <lennyaiko17@gmail.com>
2025-05-25 08:59:51 +0100

    progress on stuff

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:agcnews/data/classes/latest_news_activity.dart';
 import 'package:agcnews/data/classes/top_stories_activity.dart';
 import 'package:agcnews/data/constants.dart';
 import 'package:agcnews/data/endpoints.dart';
+import 'package:agcnews/views/widgets/top_stories_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -78,85 +79,7 @@ class _HomePageState extends State<HomePage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // Top Stories
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("TOP STORIES", style: AgcTextStyle.header1),
-                  SizedBox(height: 5.0),
-                  FutureBuilder<List<TopStoriesActivity>>(
-                    future: topStories,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(
-                          child: Padding(
-                            padding: EdgeInsets.all(20.0),
-                            child: CircularProgressIndicator(),
-                          ),
-                        );
-                      } else if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        final activities = snapshot.data!;
-                        return Column(
-                          children: [
-                            for (final activity in activities)
-                              Column(
-                                children: [
-                                  SizedBox(height: 10.0),
-                                  Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Flexible(
-                                            flex: 1,
-                                            child: ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(15.0),
-                                              child: Image.network(
-                                                activity.story.bannerImage,
-                                                height: 100.0,
-                                                width: 150.0,
-                                                fit: BoxFit.cover,
-                                              ),
-                                            ),
-                                          ),
-                                          SizedBox(width: 10.0),
-                                          Expanded(
-                                            flex: 2,
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  activity
-                                                      .story
-                                                      .category
-                                                      .categoryName
-                                                      .toUpperCase(),
-                                                  style: AgcTextStyle.header2,
-                                                ),
-                                                SizedBox(height: 5.0),
-                                                Text(
-                                                  activity.story.title,
-                                                  style: AgcTextStyle.header3,
-                                                  maxLines: 3,
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                          ],
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
+              TopStoriesWidget(),
               SizedBox(height: 20.0),
               // Latest News
               Column(
---
+++ lib/views/widgets/top_stories_widget.dart
@@ -0,0 +1,106 @@
+import 'package:agcnews/data/classes/top_stories_activity.dart';
+import 'package:agcnews/data/constants.dart';
+import 'package:agcnews/data/endpoints.dart';
+import 'package:flutter/material.dart';
+
+class TopStoriesWidget extends StatefulWidget {
+  const TopStoriesWidget({super.key});
+
+  @override
+  State<TopStoriesWidget> createState() => _TopStoriesWidgetState();
+}
+
+class _TopStoriesWidgetState extends State<TopStoriesWidget> {
+  Future<List<TopStoriesActivity>>? topStories;
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  void fetchData() {
+    setState(() {
+      topStories = API.fetchTopStories();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("TOP STORIES", style: AgcTextStyle.header1),
+        SizedBox(height: 5.0),
+        FutureBuilder<List<TopStoriesActivity>>(
+          future: topStories,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              final activities = snapshot.data!;
+              return Column(
+                children: [
+                  for (final activity in activities)
+                    Column(
+                      children: [
+                        SizedBox(height: 10.0),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Flexible(
+                                  flex: 1,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    child: Image.network(
+                                      activity.story.bannerImage,
+                                      height: 100.0,
+                                      width: 150.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                Expanded(
+                                  flex: 2,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        activity.story.category.categoryName
+                                            .toUpperCase(),
+                                        style: AgcTextStyle.header2,
+                                      ),
+                                      SizedBox(height: 5.0),
+                                      Text(
+                                        activity.story.title,
+                                        style: AgcTextStyle.header3,
+                                        maxLines: 3,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                ],
+              );
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
