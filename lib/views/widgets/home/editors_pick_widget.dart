d20be51f3c3dcec8f768f3445424a1df52d966fa
lennyaiko <lennyaiko17@gmail.com>
2025-05-25 09:51:21 +0100

    chore

---
+++ lib/views/widgets/home/editors_pick_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditorsPickWidget extends StatefulWidget {
+  const EditorsPickWidget({super.key});
+
+  @override
+  State<EditorsPickWidget> createState() => _EditorsPickWidgetState();
+}
+
+class _EditorsPickWidgetState extends State<EditorsPickWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
