5059551a8a75fdb1fd61b9203a32c7d6ff9a059d
lennyaiko <lennyaiko17@gmail.com>
2025-05-26 02:39:41 +0100

    progress on stuff

--- lib/views/widgets/drawer_widget.dart
+++ lib/views/widgets/drawer_widget.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
 
-class DrawerWidget extends StatelessWidget {
+class DrawerWidget extends StatefulWidget {
   const DrawerWidget({super.key});
+
+  @override
+  State<DrawerWidget> createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  String? menuItem = 'e1';
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
+          SizedBox(height: 20.0),
           ListTile(
-            title: Text("Home"),
+            title: Text("Home", style: TextStyle(fontSize: 15.0)),
             leading: Icon(Icons.home),
             onTap: () {},
           ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Select a media type",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
           ListTile(
-            title: Text("About"),
-            leading: Icon(Icons.info),
+            title: Text("Audio", style: TextStyle(fontSize: 15.0)),
+            leading: Icon(Icons.audio_file),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text("Video", style: TextStyle(fontSize: 15.0)),
+            leading: Icon(Icons.video_camera_back),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text("Photos", style: TextStyle(fontSize: 15.0)),
+            leading: Icon(Icons.photo),
+            onTap: () {},
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Select a category",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: Text("Photos", style: TextStyle(fontSize: 15.0)),
+            leading: Icon(Icons.category),
             onTap: () {},
           ),
         ],
