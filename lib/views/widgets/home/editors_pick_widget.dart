5c9c5519068a00f048db6ec65139a3c63d71ca11
lennyaiko <lennyaiko17@gmail.com>
2025-05-26 02:13:53 +0100

    progress

--- lib/views/widgets/home/editors_pick_widget.dart
+++ lib/views/widgets/home/editors_pick_widget.dart
@@ -1,6 +1,7 @@
 import 'package:agcnews/data/classes/editors_pick_activity.dart';
 import 'package:agcnews/data/constants.dart';
 import 'package:agcnews/data/endpoints.dart';
+import 'package:agcnews/views/pages/story_page.dart';
 import 'package:flutter/material.dart';
 
 class EditorsPickWidget extends StatefulWidget {
@@ -45,80 +46,100 @@ class _EditorsPickWidgetState extends State<EditorsPickWidget> {
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Stack(
-                alignment: Alignment.topLeft,
-                children: [
-                  Hero(
-                    tag: "editorsPick",
-                    child: AspectRatio(
-                      aspectRatio: 1920 / 1080,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          firstActivity.story!.bannerImage ?? "",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+              InkWell(
+                hoverColor: Colors.transparent,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) =>
+                              StoryPage(storyId: firstActivity.story!.id!),
                     ),
-                  ),
-                  FittedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white, width: 1.0),
-                          borderRadius: BorderRadius.circular(30.0),
-                          color: Colors.black38,
-                        ),
-                        padding: EdgeInsets.all(6.0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.king_bed_rounded,
-                              color: Colors.white,
-                              size: 20.0,
-                            ),
-                            SizedBox(width: 4.0),
-                            Text(
-                              "Editor's Pick",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 13.0,
-                                fontWeight: FontWeight.bold,
+                  );
+                },
+                child: Column(
+                  children: [
+                    Stack(
+                      alignment: Alignment.topLeft,
+                      children: [
+                        Hero(
+                          tag: "editorsPick",
+                          child: AspectRatio(
+                            aspectRatio: 1920 / 1080,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: Image.network(
+                                firstActivity.story!.bannerImage ?? "",
+                                fit: BoxFit.cover,
                               ),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                        FittedBox(
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(30.0),
+                                color: Colors.black38,
+                              ),
+                              padding: EdgeInsets.all(6.0),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.king_bed_rounded,
+                                    color: Colors.white,
+                                    size: 20.0,
+                                  ),
+                                  SizedBox(width: 4.0),
+                                  Text(
+                                    "Editor's Pick",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 13.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                firstActivity.story!.title ?? "",
-                style: AgcTextStyle.header3,
-                maxLines: 2,
-              ),
-              SizedBox(height: 5.0),
-              Text(
-                firstActivity.story!.description ?? "",
-                style: AgcTextStyle.description,
-                maxLines: 3,
-              ),
-              SizedBox(height: 5.0),
-              Row(
-                children: [
-                  Icon(Icons.circle, size: 18.0, color: Colors.red[600]),
-                  SizedBox(width: 3.0),
-                  Text(
-                    firstActivity.story!.author ?? "",
-                    style: TextStyle(
-                      fontSize: 13.0,
-                      fontWeight: FontWeight.w500,
+                    SizedBox(height: 10.0),
+                    Text(
+                      firstActivity.story!.title ?? "",
+                      style: AgcTextStyle.header3,
+                      maxLines: 2,
                     ),
-                  ),
-                ],
+                    SizedBox(height: 5.0),
+                    Text(
+                      firstActivity.story!.description ?? "",
+                      style: AgcTextStyle.description,
+                      maxLines: 3,
+                    ),
+                    SizedBox(height: 5.0),
+                    Row(
+                      children: [
+                        Icon(Icons.circle, size: 18.0, color: Colors.red[600]),
+                        SizedBox(width: 3.0),
+                        Text(
+                          firstActivity.story!.author ?? "",
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               SizedBox(height: 20.0),
               Text("More Stories", style: AgcTextStyle.header1),
@@ -146,7 +167,14 @@ class _EditorsPickWidgetState extends State<EditorsPickWidget> {
                             ),
                           ),
                           onTap: () {
-                            // Handle tap on the activity
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (context) =>
+                                        StoryPage(storyId: activity.story!.id!),
+                              ),
+                            );
                           },
                         ),
                       ],
