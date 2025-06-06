54d45da9d24e0a933d5ec17ad7b13d8ea56b3a36
lennyaiko <lennyaiko17@gmail.com>
2025-06-06 21:20:29 +0100

    done with project

--- lib/views/widgets/home/footer_widget.dart
+++ lib/views/widgets/home/footer_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FooterWidget extends StatefulWidget {
   const FooterWidget({super.key});
@@ -11,9 +12,30 @@ class _FooterWidgetState extends State<FooterWidget> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "© 2025 AGC Newsnet. All Rights Reserved.",
-        style: TextStyle(color: Colors.black54),
+      child: Column(
+        children: [
+          Text(
+            "© 2025 AGC Newsnet. All Rights Reserved.",
+            style: TextStyle(color: Colors.black54),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: FaIcon(FontAwesomeIcons.facebook),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: FaIcon(FontAwesomeIcons.instagram),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: FaIcon(FontAwesomeIcons.twitter),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
